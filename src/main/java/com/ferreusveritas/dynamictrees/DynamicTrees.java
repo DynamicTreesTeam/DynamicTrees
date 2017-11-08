b8195a0b56ad4ebc7d5fa0cfd192aff6cffed641
ferreusveritas <ferreusveritas@gmail.com>
2017-11-08 10:22:43 -0500

    Bump version to 0.6.6

--- src/main/java/com/ferreusveritas/dynamictrees/DynamicTrees.java
+++ src/main/java/com/ferreusveritas/dynamictrees/DynamicTrees.java
@@ -72,14 +72,14 @@ import net.minecraftforge.registries.IForgeRegistry;
 * </p>
 * 
 * @author ferreusveritas
-* @version 0.6.5
+* @version 0.6.6
 *
 */
 @Mod(modid = DynamicTrees.MODID, version=DynamicTrees.VERSION,dependencies="after:computercraft;after:quark")
 public class DynamicTrees {
 
 	public static final String MODID = "dynamictrees";
-	public static final String VERSION = "0.6.5";
+	public static final String VERSION = "0.6.6";
 
 	public static final DynamicTreesTab dynamicTreesTab = new DynamicTreesTab(MODID);
 	
