90f86fa5347ba546e2d86a7a73ddd087cc3fce6a
ferreusveritas <ferreusveritas@gmail.com>
2017-12-17 01:18:38 -0500

    Bump Alpha version to 0.7.1d

--- src/main/java/com/ferreusveritas/dynamictrees/DynamicTrees.java
+++ src/main/java/com/ferreusveritas/dynamictrees/DynamicTrees.java
@@ -46,7 +46,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 * </p>
 * 
 * @author ferreusveritas
-* @version 0.7.1c
+* @version 0.7.1d
 *
 */
 @Mod(modid = ModConstants.MODID, version=ModConstants.VERSION, dependencies="after:computercraft;after:quark")
