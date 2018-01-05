a3c4e1c1af50321ac25d94f0f9484d1d02524f92
ferreusveritas <ferreusveritas@gmail.com>
2018-01-05 09:59:57 -0500

    Tidy up DropCreators.  Remove apple drops from Dynamic Oak and Dark Oak.

--- src/main/java/com/ferreusveritas/dynamictrees/systems/dropcreators/DropCreatorHarvest.java
+++ src/main/java/com/ferreusveritas/dynamictrees/systems/dropcreators/DropCreatorHarvest.java
@@ -3,52 +3,28 @@ package com.ferreusveritas.dynamictrees.systems.dropcreators;
 import java.util.List;
 import java.util.Random;
 
-import com.ferreusveritas.dynamictrees.api.treedata.IDropCreator;
 import com.ferreusveritas.dynamictrees.trees.Species;
 
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
-public class DropCreatorHarvest implements IDropCreator {
-
-	ResourceLocation name;
+public class DropCreatorHarvest extends DropCreator {
 	ItemStack droppedItem;
 	float rate;
 	
 	public DropCreatorHarvest(ResourceLocation name, ItemStack droppedItem, float rate) {
-		this.name = name;
+		super(name);
 		this.droppedItem = droppedItem;
 		this.rate = rate;
 	}
 	
 	@Override
-	public ResourceLocation getName() {
-		return name;
-	}
-
-	@Override
 	public List<ItemStack> getHarvestDrop(World world, Species species, BlockPos leafPos, Random random, List<ItemStack> dropList, int soilLife, int fortune) {
 		if(rate > random.nextFloat()) {
 			dropList.add(droppedItem.copy());
 		}
-		return dropList;
-	}
-
-	@Override
-	public List<ItemStack> getVoluntaryDrop(World world, Species species, BlockPos rootPos, Random random, List<ItemStack> dropList, int soilLife) {
-		return dropList;
-	}
-
-	@Override
-	public List<ItemStack> getLeavesDrop(IBlockAccess access, Species species, BlockPos breakPos, Random random, List<ItemStack> dropList, int fortune) {
-		return dropList;
-	}
-
-	@Override
-	public List<ItemStack> getLogsDrop(World world, Species species, BlockPos breakPos, Random random, List<ItemStack> dropList, int volume) {
 		return dropList;
 	}
 	
