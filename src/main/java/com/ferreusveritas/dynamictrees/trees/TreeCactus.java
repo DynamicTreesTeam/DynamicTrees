ade910885fa725afb7d7453bb3fd774c0219366c
ferreusveritas <ferreusveritas@gmail.com>
2018-01-16 09:40:24 -0500

    Cactus stuff

--- src/main/java/com/ferreusveritas/dynamictrees/trees/TreeCactus.java
+++ src/main/java/com/ferreusveritas/dynamictrees/trees/TreeCactus.java
@@ -1,5 +1,21 @@
 package com.ferreusveritas.dynamictrees.trees;
 
+import com.ferreusveritas.dynamictrees.ModConstants;
+
+import net.minecraft.util.ResourceLocation;
+
 public class TreeCactus extends DynamicTree {
+	
+	public TreeCactus() {
+		super(new ResourceLocation(ModConstants.MODID, "cactus"), -1);
+	}
+	
+	public class speciesCactus extends Species {
+		
+		public speciesCactus(DynamicTree treeFamily) {
+			super(treeFamily.getName(), treeFamily);
+		}
+		
+	}
 	
 }
