0388583943cb8603241148107496df480b5bca6c
ferreusveritas <ferreusveritas@gmail.com>
2018-02-09 16:44:05 -0500

    Dendrocoil tileentity overhaul

--- src/main/java/com/ferreusveritas/dynamictrees/tileentity/TileEntityDendroCoil.java
+++ src/main/java/com/ferreusveritas/dynamictrees/tileentity/TileEntityDendroCoil.java
@@ -11,74 +11,98 @@ import dan200.computercraft.api.peripheral.IComputerAccess;
 import dan200.computercraft.api.peripheral.IPeripheral;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ITickable;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class TileEntityDendroCoil extends TileEntity implements IPeripheral, ITickable {
 
 	public enum ComputerMethod {
-		growPulse,
-		getCode,
-		setCode,
-		getTree,
-		plantTree,
-		killTree,
-		getSoilLife,
-		setSoilLife,
-		getSpeciesList,
-		createStaff,
-		testPoisson,
-		testPoisson2,
-		testPoisson3
+		growPulse("", true),
+		getCode("", false),
+		setCode("ss", true, "treeName", "joCode"),
+		getTree("", false),
+		plantTree("s", true, "treeName"),
+		killTree("", true),
+		getSoilLife("", false),
+		setSoilLife("n", true, "life"),
+		getSpeciesList("", false),
+		createStaff("sssb", true, "treeName", "joCode", "rgbColor", "readOnly"),
+		testPoisson("nnn", true, "radius1", "radius2", "angle"),
+		testPoisson2("nnnn", true, "radius1", "radius2", "angle", "radius3", "onlyTight"),
+		testPoisson3("nnnnnb", true, "radius1", "delX", "delZ", "radius2", "radius3", "onlyTight");
+		
+		private final String argTypes;
+		private final String args[];
+		private final boolean cached;
+		
+		private ComputerMethod(String argTypes, boolean cached, String ... args) {
+			this.argTypes = argTypes;
+			this.args = args;
+			this.cached = cached;
+		}
+		
+		public boolean isCached() {
+			return cached;
+		}
+		
+		public boolean isValidArguments(Object[] arguments) {
+			if(arguments.length >= argTypes.length()) {
+				for (int i = 0; i < argTypes.length(); i++){
+					if(!CCDataType.byIdent(argTypes.charAt(i)).isInstance(arguments[i])) {
+						return false;
+					}
+				}
+				return true;
+			}
+			return false;
+		}
+		
+		public boolean validateArguments(Object[] arguments) throws LuaException {
+			if(isValidArguments(arguments)) {
+				return true;
+			}
+			throw new LuaException(invalidArgumentsError());
+		}
+		
+		public String invalidArgumentsError() {
+			String error = "Expected: " + this.toString();
+			for (int i = 0; i < argTypes.length(); i++){
+				error += " " + args[i] + "<" + CCDataType.byIdent(argTypes.charAt(i)).name + ">";
+			}
+			return error;
+		}
 	}
 	
 	private class CachedCommand {
 		ComputerMethod method;
 		Object[] arguments;
 		int argRead = 0;
-
+		
 		public CachedCommand(int method, Object[] args) {
 			this.method = ComputerMethod.values()[method];
 			this.arguments = args;
 		}
 		
 		public double d() {
-			return d(argRead++);
+			return ((Double)arguments[argRead++]).doubleValue();
 		}
-
+		
 		public int i() {
-			return i(argRead++);
+			return ((Double)arguments[argRead++]).intValue();
 		}
-
+		
 		public String s() {
-			return s(argRead++);
+			return ((String)arguments[argRead++]);
 		}
-
+		
 		public boolean b() {
-			return b(argRead++);
-		}
-		
-		public double d(int arg) {
-			return ((Double)arguments[arg]).doubleValue();
-		}
-		
-		public int i(int arg) {
-			return ((Double)arguments[arg]).intValue();
-		}
-		
-		public String s(int arg) {
-			return ((String)arguments[arg]);
-		}
-		
-		public boolean b(int arg) {
-			return ((Boolean)arguments[arg]).booleanValue();
+			return ((Boolean)arguments[argRead++]).booleanValue();
 		}
 	}
-
+	
 	private ArrayList<CachedCommand> cachedCommands = new ArrayList<CachedCommand>(1);
 	private String treeName;
 	private int soilLife;
-
+	
 	public static final int numMethods = ComputerMethod.values().length;
 	public static final String[] methodNames = new String[numMethods]; 
 	static {
@@ -86,20 +110,16 @@ public class TileEntityDendroCoil extends TileEntity implements IPeripheral, ITi
 			methodNames[method.ordinal()] = method.toString(); 
 		}
 	}
-
+	
 	public void cacheCommand(int method, Object[] args) {
 		synchronized (cachedCommands) {
 			cachedCommands.add(new CachedCommand(method, args));
 		}
 	}
-
-	public BlockPos getPos() {
-		return pos;
-	}
-
+	
 	@Override
 	public void update() {
-
+		
 		BlockDendroCoil dendroCoil = (BlockDendroCoil)getBlockType();
 		World world = getWorld();
 		
@@ -107,7 +127,7 @@ public class TileEntityDendroCoil extends TileEntity implements IPeripheral, ITi
 			treeName = new String(dendroCoil.getSpecies(world, getPos()));
 			soilLife = dendroCoil.getSoilLife(world, getPos());
 		}
-
+		
 		//Run commands that are cached that shouldn't be in the lua thread
 		synchronized(cachedCommands) {
 			if(cachedCommands.size() > 0) { 
@@ -136,133 +156,65 @@ public class TileEntityDendroCoil extends TileEntity implements IPeripheral, ITi
 	public String getType() {
 		return "dendrocoil";
 	}
-
+	
 	@Override
 	public String[] getMethodNames() {
 		return methodNames;
 	}
-
+	
 	/**
 	* I hear ya Dan!  Make the function threadsafe by caching the commmands to run in the main world server thread and not the lua thread.
 	*/
 	@Override
-	public Object[] callMethod(IComputerAccess computer, ILuaContext context, int method, Object[] arguments) throws LuaException {
-		if(method < 0 || method >= numMethods) {
+	public Object[] callMethod(IComputerAccess computer, ILuaContext context, int methodNum, Object[] arguments) throws LuaException {
+		if(methodNum < 0 || methodNum >= numMethods) {
 			throw new IllegalArgumentException("Invalid method number");
 		}
-
+		
 		BlockDendroCoil dendroCoil = (BlockDendroCoil)getBlockType();
 		World world = getWorld();
 		
 		if(!world.isRemote && dendroCoil != null) {
-			switch(ComputerMethod.values()[method]) {
-				case getCode:
-					return new Object[]{ dendroCoil.getCode(world, getPos()) };
-				case getTree:
-					synchronized(this) {
-						return new Object[]{treeName};
-					}
-				case plantTree:
-					if(arguments.length >= 1 &&
-						arguments[0] instanceof String) {
-						cacheCommand(method, arguments);
-					} else {
-						throw new LuaException("Expected: " + methodNames[method] + " treeName<String>");
-					}
-					break;
-				case getSoilLife:
-					synchronized(this) {
-						return new Object[]{soilLife};
-					}
-				case setSoilLife:
-					if(arguments.length >= 1 &&
-						arguments[0] instanceof Double) {
-						cacheCommand(method, arguments);
-					} else {
-						throw new LuaException("Expected: " + methodNames[method] + " life<Number>");
-					}
-					break;
-				case getSpeciesList:
-					ArrayList<String> species = new ArrayList<String>();
-					TreeRegistry.getSpeciesDirectory().forEach(r -> species.add(r.toString()));
-					return species.toArray();
-				case createStaff:
-					if(arguments.length >= 4 &&
-						arguments[0] instanceof String &&
-						arguments[1] instanceof String &&
-						arguments[2] instanceof String &&
-						arguments[3] instanceof Boolean) {
-						cacheCommand(method, arguments);
-					} else {
-						throw new LuaException("Expected: " + methodNames[method] + " treeName<String>, joCode<String>, rgbColor<String>, readOnly<Boolean>");
-					}
-					break;
-				case setCode:
-					if(arguments.length >= 2 && 
-						arguments[0] instanceof String &&
-						arguments[1] instanceof String) {
-						cacheCommand(method, arguments);
-					} else {
-						throw new LuaException("Expected: " + methodNames[method] + " treeName<String>, joCode<String>");
-					}
-					break;
-				case growPulse:
-					cacheCommand(method, arguments);
-					break;
-				case killTree:
-					cacheCommand(method, arguments);
-					break;
-				case testPoisson:
-					if(arguments.length >= 3 &&
-						arguments[0] instanceof Double &&
-						arguments[1] instanceof Double &&
-						arguments[2] instanceof Double) {
-						cacheCommand(method, arguments);
-					} else {
-						throw new LuaException("Expected: " + methodNames[method] + " radius1<Number>, radius2<Number>, angle<Number>");
-					}
-					break;
-				case testPoisson2:
-					if(arguments.length >= 4 &&
-						arguments[0] instanceof Double &&
-						arguments[1] instanceof Double &&
-						arguments[2] instanceof Double &&
-						arguments[3] instanceof Double) {
-						cacheCommand(method, arguments);
-					} else {
-						throw new LuaException("Expected: " + methodNames[method] + " radius1<Number>, radius2<Number>, angle<Number>, radius3<Number>, onlyTight<Boolean>");
-					}
-				case testPoisson3:
-					if(arguments.length >= 4 &&
-						arguments[0] instanceof Double &&
-						arguments[1] instanceof Double &&
-						arguments[2] instanceof Double &&
-						arguments[3] instanceof Double &&
-						arguments[4] instanceof Double) {
-						cacheCommand(method, arguments);
-					} else {
-						throw new LuaException("Expected: " + methodNames[method] + " radius1<Number>, delX<Number>, delZ<Number>, radius2<Number>, radius3<Number>, onlyTight<Boolean>");
-					}
-					break;
-				default:
-					break;
+			ComputerMethod method = ComputerMethod.values()[methodNum];
+
+			if(method.validateArguments(arguments)) {
+				switch(method) {
+					case getCode:
+						return new Object[]{ dendroCoil.getCode(world, getPos()) };
+					case getTree:
+						synchronized(this) {
+							return new Object[]{treeName};
+						}
+					case getSoilLife:
+						synchronized(this) {
+							return new Object[]{soilLife};
+						}
+					case getSpeciesList:
+						ArrayList<String> species = new ArrayList<String>();
+						TreeRegistry.getSpeciesDirectory().forEach(r -> species.add(r.toString()));
+						return species.toArray();
+					default:
+						if(method.isCached()) {
+							cacheCommand(methodNum, arguments);
+						}
+				}
 			}
 		}
-
+		
 		return null;
 	}
-
+	
 	@Override
 	public void attach(IComputerAccess computer) {
 	}
-
+	
 	@Override
 	public void detach(IComputerAccess computer) {
 	}
-
+	
 	@Override
 	public boolean equals(IPeripheral other) {
 		return this == other;
 	}
-
+	
 }
