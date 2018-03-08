e26999847b28e85be3bd6bb267b288de025ae550
ferreusveritas <ferreusveritas@gmail.com>
2018-03-08 09:28:02 -0500

    Remove unused imports

--- src/main/java/com/ferreusveritas/dynamictrees/blocks/BlockRootyDirt.java
+++ src/main/java/com/ferreusveritas/dynamictrees/blocks/BlockRootyDirt.java
@@ -1,20 +1,10 @@
 package com.ferreusveritas.dynamictrees.blocks;
 
-import java.util.Random;
-
-import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.client.particle.ParticleDigging;
-import net.minecraft.client.particle.ParticleManager;
 import net.minecraft.util.BlockRenderLayer;
-import net.minecraft.util.EnumParticleTypes;
-import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.IBlockAccess;
-import net.minecraft.world.World;
-import net.minecraftforge.common.property.IExtendedBlockState;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
