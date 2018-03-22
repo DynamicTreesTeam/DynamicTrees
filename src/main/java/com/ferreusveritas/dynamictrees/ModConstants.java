8d69ecf267422cf2fad8ebd6c3ed0d58e03dfdb6
ferreusveritas <ferreusveritas@gmail.com>
2018-03-22 09:15:45 -0400

    Update forge to 14.23.2.2611

--- src/main/java/com/ferreusveritas/dynamictrees/ModConstants.java
+++ src/main/java/com/ferreusveritas/dynamictrees/ModConstants.java
@@ -3,7 +3,7 @@ package com.ferreusveritas.dynamictrees;
 public class ModConstants {
 
 	public static final String MODID = "dynamictrees";
-	public static final String VERSION = "@VERSION@";
-	//public static final String VERSION = "1.12.2-0.7.5b";
+	//public static final String VERSION = "@VERSION@";
+	public static final String VERSION = "1.12.2-0.7.5b";
 	
 }
