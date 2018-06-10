ac235064febc2dc4e37e9e48861b94c58c7f7ffe
ferreusveritas <ferreusveritas@gmail.com>
2018-06-10 13:51:39 -0400

    cleanup dev

--- src/main/java/com/ferreusveritas/dynamictrees/ModConstants.java
+++ src/main/java/com/ferreusveritas/dynamictrees/ModConstants.java
@@ -4,7 +4,7 @@ public class ModConstants {
 
 	public static final String MODID = "dynamictrees";
 	public static final String VERSION = "@VERSION@";
-	//public static final String VERSION = "1.12.2-0.7.7e";
+	//public static final String VERSION = "1.12.2-0.7.8";
 	
 	public static final String AFTER = "after:";
 	public static final String BEFORE = "before:";
