72c7e4df4db04acb08972724bb588a8863cf837c
ferreusveritas <ferreusveritas@gmail.com>
2018-07-02 18:35:34 -0400

    Moved Dendrocoil to Magic Cracker Factory mod.

--- src/main/java/com/ferreusveritas/dynamictrees/compat/CommonProxyCompat.java
+++ src/main/java/com/ferreusveritas/dynamictrees/compat/CommonProxyCompat.java
@@ -9,28 +9,19 @@ import net.minecraftforge.registries.IForgeRegistry;
 
 public class CommonProxyCompat {
 
-	public CCProxyBase ccproxy;
-	
 	public void preInit() {
 		BlockDynamicLeaves.passableLeavesModLoaded = net.minecraftforge.fml.common.Loader.isModLoaded("passableleaves");
-		
-		//Computercraft Creative Mode Stuff
-		ccproxy = CCProxyBase.hasComputerCraft() ? new CCProxyActive() : new CCProxyBase();
-		ccproxy.createBlocks();
 	}
 
 	public void init() {}
 	
 	public void registerBlocks(IForgeRegistry<Block> registry) {
-		ccproxy.registerBlocks(registry);
 	}
 	
 	public void registerItems(IForgeRegistry<Item> registry) {
-		ccproxy.registerItems(registry);		
 	}
 
 	public void registerRecipes(IForgeRegistry<IRecipe> registry) {
-		ccproxy.registerRecipes(registry);
 	}
 	
 }
