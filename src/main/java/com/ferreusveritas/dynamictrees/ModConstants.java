c1d2a2bb0f97d9c32b018696fc0c7df73f3442cc
ferreusveritas <ferreusveritas@gmail.com>
2018-09-11 10:55:00 -0400

    update dttc dependency info

--- src/main/java/com/ferreusveritas/dynamictrees/ModConstants.java
+++ src/main/java/com/ferreusveritas/dynamictrees/ModConstants.java
@@ -6,7 +6,7 @@ public class ModConstants {
 	public static final String NAME = "Dynamic Trees";
 	public static final String VERSIONDEV = "1.12.2-9.9.9z";
 	public static final String VERSIONAUTO = "@VERSION@";
-	public static final String VERSION = VERSIONDEV;//Change to DEV in development, AUTO for release
+	public static final String VERSION = VERSIONAUTO;//Change to DEV in development, AUTO for release
 	
 	public static final String AFTER = "after:";
 	public static final String BEFORE = "before:";
@@ -21,7 +21,7 @@ public class ModConstants {
 	
 	//Other Mods
 	public static final String DYNAMICTREESBOP_VER = "@[1.3.1b,)";
-	public static final String DYNAMICTREESTC_VER = "@[1.0,)";
+	public static final String DYNAMICTREESTC_VER = "@[1.1.1a,)";
 	
 	public static final String DEPENDENCIES
 		= BEFORE + DYNAMICTREESBOP + DYNAMICTREESBOP_VER
