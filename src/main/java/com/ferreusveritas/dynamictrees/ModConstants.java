39f96a164ae50bc82e4173a46f50c3a58e57d2d8
ferreusveritas <ferreusveritas@gmail.com>
2018-09-11 15:46:22 -0400

    versioning work

--- src/main/java/com/ferreusveritas/dynamictrees/ModConstants.java
+++ src/main/java/com/ferreusveritas/dynamictrees/ModConstants.java
@@ -4,24 +4,28 @@ public class ModConstants {
 	
 	public static final String MODID = "dynamictrees";
 	public static final String NAME = "Dynamic Trees";
-	public static final String VERSIONDEV = "1.12.2-9.9.9z";
-	public static final String VERSIONAUTO = "@VERSION@";
-	public static final String VERSION = VERSIONAUTO;//Change to DEV in development, AUTO for release
+	public static final String VERSIONDEV = "1.12.2-9.9.9z";//Maxed out version to satisfy dependencies during dev
+	public static final String VERSIONAUTO = "@VERSION@";//Assigned from gradle during build
+	public static final String VERSION = VERSIONAUTO;//Change to VERSIONDEV in development, VERSIONAUTO for release
 	
 	public static final String AFTER = "after:";
 	public static final String BEFORE = "before:";
+	public static final String REQAFTER = "required-after:";
+	public static final String REQBEFORE = "required-before:";
 	public static final String NEXT = ";";
+	public static final String AT = "@[";
+	public static final String ORGREATER = ",)";
 	
 	//Other mods can use this string to depend on the latest version of Dynamic Trees
-	public static final String DYNAMICTREES_LATEST = MODID + "@[" + VERSION + ",)";
+	public static final String DYNAMICTREES_LATEST = MODID + AT + VERSION + ORGREATER;
 	
 	//Other Mods
 	public static final String DYNAMICTREESBOP = "dynamictreesbop";
 	public static final String DYNAMICTREESTC = "dynamictreestc";
 	
-	//Other Mods
-	public static final String DYNAMICTREESBOP_VER = "@[1.3.1b,)";
-	public static final String DYNAMICTREESTC_VER = "@[1.1.1a,)";
+	//Other Mod Versions
+	public static final String DYNAMICTREESBOP_VER = AT + "1.3.1b" + ORGREATER;
+	public static final String DYNAMICTREESTC_VER =  AT + "1.1.1a" + ORGREATER;
 	
 	public static final String DEPENDENCIES
 		= BEFORE + DYNAMICTREESBOP + DYNAMICTREESBOP_VER
