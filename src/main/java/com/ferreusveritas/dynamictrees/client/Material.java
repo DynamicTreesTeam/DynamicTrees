fb754c6155207d85c00a3b0fbdc63a07e2fa88ff
ferreusveritas <ferreusveritas@gmail.com>
2018-10-15 08:02:09 -0400

    Import some tinkers construct texture code for research

---
+++ src/main/java/com/ferreusveritas/dynamictrees/client/Material.java
@@ -0,0 +1,9 @@
+package com.ferreusveritas.dynamictrees.client;
+
+public class Material {
+
+	public String identifier;
+	public MaterialRenderInfo renderInfo;
+	
+	
+}
---
+++ src/main/java/com/ferreusveritas/dynamictrees/client/MaterialRenderInfo.java
@@ -0,0 +1,22 @@
+package com.ferreusveritas.dynamictrees.client;
+
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.util.ResourceLocation;
+
+public class MaterialRenderInfo {
+
+	public String getTextureSuffix() {
+		return "suffix";
+	}
+
+	public TextureAtlasSprite getTexture(ResourceLocation baseTexture, String location) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean isStitched() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+}
