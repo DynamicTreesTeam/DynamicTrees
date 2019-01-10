c1508eaa5d5dd4053598fe73840576e7a5dca366
ferreusveritas <ferreusveritas@gmail.com>
2019-01-10 16:03:44 -0500

    This should allow FML to understand what I mean

--- src/main/java/com/ferreusveritas/dynamictrees/ModConstants.java
+++ src/main/java/com/ferreusveritas/dynamictrees/ModConstants.java
@@ -25,17 +25,14 @@ public class ModConstants {
 	public static final String DYNAMICTREESTRAVERSE = "dttraverse";
 	public static final String DYNAMICTREESHNC = "dynamictreeshnc";
 	public static final String RUSTIC = "rustic";
-	
-	//A standardized mod version for team controlled "official" add-ons
-	public static final String STANDARD_ADDON_VER = "1.4.1e";
-	
+		
 	//Other Mod Versions.. These have been added to avoid the whole "Duh.. How come my mod is crashing?" bullshit bug reports.
-	public static final String DYNAMICTREESBOP_VER = AT + STANDARD_ADDON_VER + ORGREATER;
-	public static final String DYNAMICTREESTC_VER =	 AT + STANDARD_ADDON_VER + ORGREATER;
-	public static final String DYNAMICTREESPHC_VER = AT + STANDARD_ADDON_VER + ORGREATER;
+	public static final String DYNAMICTREESBOP_VER = GREATERTHAN + "1.4.1d" + ORGREATER;
+	public static final String DYNAMICTREESTC_VER =	 GREATERTHAN + "1.4.1d" + ORGREATER;
+	public static final String DYNAMICTREESPHC_VER = GREATERTHAN + "1.4.1e" + ORGREATER;
 	public static final String DYNAMICTREESTRAVERSE_VER =  GREATERTHAN + "1.4" + ORGREATER;//Traverse will need a new build. Display an error rather than crash.
 	public static final String DYNAMICTREESHNC_VER =  GREATERTHAN + "1.1" + ORGREATER;//Heat and Climate Add-on has not be updated in a while and the latest 1.1 is not longer supported
-	public static final String RUSTIC_VER = AT + "1.0.14" + ORGREATER;
+	public static final String RUSTIC_VER = GREATERTHAN + "1.0.14" + ORGREATER;
 	
 	//Forge
 	private static final String FORGE = "forge";
