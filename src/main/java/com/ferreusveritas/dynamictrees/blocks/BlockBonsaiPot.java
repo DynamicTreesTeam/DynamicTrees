0bb99ac379c9e224b9e4fad5d21782675b04690a
ferreusveritas <ferreusveritas@gmail.com>
2019-01-10 22:52:53 -0500

    Fix bonsai pot block face shape

--- src/main/java/com/ferreusveritas/dynamictrees/blocks/BlockBonsaiPot.java
+++ src/main/java/com/ferreusveritas/dynamictrees/blocks/BlockBonsaiPot.java
@@ -12,6 +12,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.BlockFlowerPot;
 import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.item.EntityItem;
@@ -221,6 +222,11 @@ public class BlockBonsaiPot extends BlockContainer {
 	@Override
 	public EnumBlockRenderType getRenderType(IBlockState state) {
 		return EnumBlockRenderType.MODEL;
+	}
+	
+	@Override
+	public BlockFaceShape getBlockFaceShape(IBlockAccess worldIn, IBlockState state, BlockPos pos, EnumFacing face) {
+		return face == EnumFacing.DOWN ? BlockFaceShape.CENTER_SMALL : BlockFaceShape.UNDEFINED;
 	}
 	
 }
