a90a6968afa65b9edeedfdca7ee2b39515655359
Ferreusveritas <ferreusveritas@gmail.com>
2020-09-13 23:19:58 -0400

    Bark less about missing JoCode files

--- src/main/java/com/ferreusveritas/dynamictrees/worldgen/JoCodeStore.java
+++ src/main/java/com/ferreusveritas/dynamictrees/worldgen/JoCodeStore.java
@@ -7,7 +7,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -15,7 +14,6 @@ import java.util.logging.Logger;
 import com.ferreusveritas.dynamictrees.ModConstants;
 import com.ferreusveritas.dynamictrees.trees.Species;
 
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.MathHelper;
 
 /**
@@ -60,9 +58,8 @@ public class JoCodeStore {
 			} else {
 				throw(new FileNotFoundException(filename));
 			}
-			
 		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+			Logger.getLogger(ModConstants.MODID).log(Level.WARNING, "No JoCode file found for species \"" + species + "\" at location: " + filename);
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -72,35 +69,10 @@ public class JoCodeStore {
 
 	public void addCode(Species species, int radius, String code) {
 		JoCode joCode = species.getJoCode(code).setCareful(false);
-		
-		//Code reserved for collecting WorldGen JoCodes
-		//collectWorldGenCodes(tree, radius, joCode);
-
 		getListForRadius(radius).add(joCode);
 	}
-
-	/**
-	 * This collects a list of trees and creates 4 variations for the 4 directions and then
-	 * sorts them alphanumerically.  By sorting the rotated JoCodes you can eliminate duplicates
-	 * who are only different by the direction they are facing.
-	 * 
-	 * @param radius
-	 * @param joCode
-	 */
-	//Code reserved for collecting WorldGen JoCodes
-	@SuppressWarnings("unused")
-	private void collectWorldGenCodes(int radius, JoCode joCode) {
-		EnumFacing dirs[] = {EnumFacing.NORTH, EnumFacing.SOUTH, EnumFacing.WEST, EnumFacing.EAST};
-		ArrayList<String> arr = new ArrayList<String>() ;
-		for(EnumFacing dir: dirs) {
-			arr.add(joCode.rotate(dir).toString());
-		}
-		
-		Collections.sort(arr);
-		System.out.println(species + ":" + radius + ":" + arr.get(0));
-	}
 	
-	public JoCode getRandomCode(int radius, Random rand) {		
+	public JoCode getRandomCode(int radius, Random rand) {
 		ArrayList<JoCode> list = getListForRadius(radius);
 		if(!list.isEmpty()) {
 			return list.get(rand.nextInt(list.size()));
