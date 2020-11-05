8adde64444d1d860fc2eba03a06fa838acff18c1
Ferreusveritas <ferreusveritas@gmail.com>
2020-11-05 13:18:37 -0500

    Remove some hopelessly out of date or obsolete classes

--- src/main/java/com/ferreusveritas/dynamictrees/event/CommonEventHandler.java
+++ src/main/java/com/ferreusveritas/dynamictrees/event/CommonEventHandler.java
@@ -2,7 +2,6 @@ package com.ferreusveritas.dynamictrees.event;
 
 import com.ferreusveritas.dynamictrees.api.WorldGenRegistry;
 import com.ferreusveritas.dynamictrees.init.DTClient;
-import com.ferreusveritas.dynamictrees.seasons.SeasonManager;
 
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.world.WorldEvent;
@@ -10,39 +9,32 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.LogicalSide;
 
 public class CommonEventHandler {
-
-	SeasonManager seasonManager = new SeasonManager();
-
-//	@SubscribeEvent
-//	public void onLoadComplete(FMLLoadCompleteEvent event){
-//		DTClient.discoverWoodColors();
-//	}
-
+	
+	//	@SubscribeEvent
+	//	public void onLoadComplete(FMLLoadCompleteEvent event){
+	//		DTClient.discoverWoodColors();
+	//	}
+	
 	@SubscribeEvent
 	public void onWorldTick(TickEvent.WorldTickEvent event) {
-
 		if(event.side == LogicalSide.SERVER) {
 			FutureBreak.process(event.world);
 		}
-
-		if(event.type == TickEvent.Type.WORLD && event.phase == TickEvent.Phase.START && event.world.getDimension().isSurfaceWorld()) {
-			seasonManager.updateTick(event.world, event.world.getGameTime());
-		}
 	}
-
+	
 	@SubscribeEvent
 	public void onWorldLoad(WorldEvent.Load event) {
 		if (event.getWorld().isRemote()){
 			DTClient.discoverWoodColors();
 		}
-
+		
 		if(WorldGenRegistry.isWorldGenEnabled() && !event.getWorld().isRemote()) {
 			if(!WorldGenRegistry.validateBiomeDataBases()) {
 				WorldGenRegistry.populateDataBase();
 			}
 		}
-
-//		event.getWorld().addEventListener(new WorldListener(event.getWorld(), event.getWorld().ser()));
+		
+		//		event.getWorld().addEventListener(new WorldListener(event.getWorld(), event.getWorld().ser()));
 	}
-
+	
 }
