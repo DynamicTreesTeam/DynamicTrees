052e46768c7ba30d3d72eb87f55db465d1d9cb67
Harleyoc1 <harley.oconnor@icloud.com>
2021-02-17 16:45:55 +0000

    (some changes didn't commit, oops)

--- src/main/java/com/ferreusveritas/dynamictrees/api/worldgen/IBiomeDataBasePopulator.java
+++
@@ -1,9 +0,0 @@
-package com.ferreusveritas.dynamictrees.api.worldgen;
-
-import com.ferreusveritas.dynamictrees.worldgen.BiomeDataBase;
-
-public interface IBiomeDataBasePopulator {
-
-	void populate(BiomeDataBase biomeDataBase);
-
-}
