347046642ad3879bb1fd391f3c31e1826caf8090
harleyoconnor <harleyoc1@gmail.com>
2021-05-09 15:37:22 +0100

    Removed valid leaves and branches from tree packs.

--- src/main/java/com/ferreusveritas/dynamictrees/trees/FamilyManager.java
+++ src/main/java/com/ferreusveritas/dynamictrees/trees/FamilyManager.java
@@ -2,7 +2,6 @@ package com.ferreusveritas.dynamictrees.trees;
 
 import com.ferreusveritas.dynamictrees.api.TreeRegistry;
 import com.ferreusveritas.dynamictrees.api.treepacks.JsonApplierRegistryEvent;
-import com.ferreusveritas.dynamictrees.blocks.branches.BranchBlock;
 import com.ferreusveritas.dynamictrees.blocks.leaves.LeavesProperties;
 import com.ferreusveritas.dynamictrees.resources.JsonRegistryEntryReloadListener;
 import com.ferreusveritas.dynamictrees.util.json.JsonHelper;
@@ -47,8 +46,7 @@ public final class FamilyManager extends JsonRegistryEntryReloadListener<Family>
             registryName = TreeRegistry.processResLoc(registryName);
             Species.REGISTRY.runOnNextLock(Species.REGISTRY.generateIfValidRunnable(registryName, family::setCommonSpecies, setCommonWarn(family, registryName)));
         }).register("conifer_variants", Boolean.class, Family::setHasConiferVariants)
-                .register("can_support_cocoa", Boolean.class, Family::setCanSupportCocoa)
-                .registerArrayApplier("valid_branches", BranchBlock.class, Family::addValidBranches);
+                .register("can_support_cocoa", Boolean.class, Family::setCanSupportCocoa);
 
         super.registerAppliers();
     }
