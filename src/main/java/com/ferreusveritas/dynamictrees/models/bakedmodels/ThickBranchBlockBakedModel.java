8ac851dd23e66b0d82b215590720116453cde28a
supermassimo <massimocantugarcia@gmail.com>
2021-05-18 12:50:43 -0300

    reversed change that made thick branches invisible

--- src/main/java/com/ferreusveritas/dynamictrees/models/bakedmodels/ThickBranchBlockBakedModel.java
+++ src/main/java/com/ferreusveritas/dynamictrees/models/bakedmodels/ThickBranchBlockBakedModel.java
@@ -4,6 +4,7 @@ import com.ferreusveritas.dynamictrees.blocks.branches.BranchBlock;
 import com.ferreusveritas.dynamictrees.blocks.branches.ThickBranchBlock;
 import com.ferreusveritas.dynamictrees.client.ModelUtils;
 import com.ferreusveritas.dynamictrees.models.modeldata.ModelConnections;
+import com.ferreusveritas.dynamictrees.trees.Family;
 import com.ferreusveritas.dynamictrees.util.CoordUtils;
 import com.ferreusveritas.dynamictrees.util.CoordUtils.Surround;
 import com.google.common.collect.Maps;
@@ -22,9 +23,9 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.model.data.IModelData;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
 
 @OnlyIn(Dist.CLIENT)
 public class ThickBranchBlockBakedModel extends BasicBranchBlockBakedModel {
@@ -51,60 +52,56 @@ public class ThickBranchBlockBakedModel extends BasicBranchBlockBakedModel {
 
 		TextureAtlasSprite thickRingsTexture = ModelUtils.getTexture(this.thickRingsResLoc);
 
-		//if (isTextureNull(thickRingsTexture)) {
-			//thickRingsTexture = ThickRingTextureManager.uploader.getTextureAtlas().getSprite(thickRingsResLoc);
-			//thickRingsTexture = ModelUtils.getTexture(thickRingsResLoc, ThickRingTextureManager.LOCATION_THICKRINGS_TEXTURE);
+		//if (isTextureNull(thickRingsTexture)){
+		//thickRingsTexture = ThickRingTextureManager.uploader.getTextureAtlas().getSprite(thickRingsResLoc);
+		//thickRingsTexture = ModelUtils.getTexture(thickRingsResLoc, ThickRingTextureManager.LOCATION_THICKRINGS_TEXTURE);
 
-			if (isTextureNull(thickRingsTexture)){
-				thickRingsTexture = this.ringsTexture;
-			}
+		if (isTextureNull(thickRingsTexture)){
+			thickRingsTexture = this.ringsTexture;
+		}
 		//}
-		
+
 		for (int i = 0; i < ThickBranchBlock.MAX_RADIUS_TICK -ThickBranchBlock.MAX_RADIUS; i++) {
-			final int radius = i + ThickBranchBlock.MAX_RADIUS + 1;
-			this.trunksBark[i] = this.bakeTrunkBark(radius, this.barkTexture, true);
-			this.trunksTopBark[i] = this.bakeTrunkBark(radius, this.barkTexture, false);
-			this.trunksTopRings[i] = this.bakeTrunkRings(radius,  thickRingsTexture, Direction.UP);
-			this.trunksBotRings[i] = this.bakeTrunkRings(radius,  thickRingsTexture, Direction.DOWN);
+			int radius = i + ThickBranchBlock.MAX_RADIUS + 1;
+			trunksBark[i] = bakeTrunkBark(radius, this.barkTexture, true);
+			trunksTopBark[i] = bakeTrunkBark(radius, this.barkTexture, false);
+			trunksTopRings[i] = bakeTrunkRings(radius,  thickRingsTexture, Direction.UP);
+			trunksBotRings[i] = bakeTrunkRings(radius,  thickRingsTexture, Direction.DOWN);
 		}
 	}
 
 	public IBakedModel bakeTrunkBark(int radius, TextureAtlasSprite bark, boolean side) {
 
-		final SimpleBakedModel.Builder builder = new SimpleBakedModel.Builder(this.blockModel.customData, ItemOverrideList.EMPTY).particle(bark);
-		final AxisAlignedBB wholeVolume = new AxisAlignedBB(8 - radius, 0, 8 - radius, 8 + radius, 16, 8 + radius);
+		SimpleBakedModel.Builder builder = new SimpleBakedModel.Builder(this.blockModel.customData, ItemOverrideList.EMPTY).particle(bark);
+		AxisAlignedBB wholeVolume = new AxisAlignedBB(8 - radius, 0, 8 - radius, 8 + radius, 16, 8 + radius);
 
 		final Direction[] run = side ? CoordUtils.HORIZONTALS : new Direction[] { Direction.UP, Direction.DOWN };
-		final ArrayList<Vector3i> offsets = new ArrayList<>();
+		ArrayList<Vector3i> offsets = new ArrayList<>();
 
 		for (Surround dir: Surround.values()) {
-			offsets.add(dir.getOffset()); // 8 surrounding component pieces.
+			offsets.add(dir.getOffset()); // 8 surrounding component pieces
 		}
-		offsets.add(new Vector3i(0, 0, 0)); // Center
+		offsets.add(new Vector3i(0, 0, 0));//Center
 
-		for (final Direction face: run) {
+		for (Direction face: run) {
 			final Vector3i dirVector = face.getNormal();
 
 			for (Vector3i offset : offsets) {
-				if (face.getAxis() != Axis.Y ||
-						new Vector3d(dirVector.getX(), dirVector.getY(), dirVector.getZ())
-								.add(new Vector3d(offset.getX(), offset.getY(), offset.getZ()))
-								.lengthSqr() <= 2.25)
-					continue;
+				if (face.getAxis() == Axis.Y || new Vector3d(dirVector.getX(), dirVector.getY(), dirVector.getZ()).add(new Vector3d(offset.getX(), offset.getY(), offset.getZ())).lengthSqr() > 2.25) { //This means that the dir and face share a common direction
+					Vector3d scaledOffset = new Vector3d(offset.getX() * 16, offset.getY() * 16, offset.getZ() * 16);//Scale the dimensions to match standard minecraft texels
+					AxisAlignedBB partBoundary = new AxisAlignedBB(0, 0, 0, 16, 16, 16).move(scaledOffset).intersect(wholeVolume);
 
-				// This means that the dir and face share a common direction.
-				final Vector3d scaledOffset = new Vector3d(offset.getX() * 16, offset.getY() * 16, offset.getZ() * 16);//Scale the dimensions to match standard minecraft texels
-				final AxisAlignedBB partBoundary = new AxisAlignedBB(0, 0, 0, 16, 16, 16).move(scaledOffset).intersect(wholeVolume);
+					Vector3f[] limits = ModelUtils.AABBLimits(partBoundary);
 
-				final Vector3f[] limits = ModelUtils.AABBLimits(partBoundary);
+					Map<Direction, BlockPartFace> mapFacesIn = Maps.newEnumMap(Direction.class);
 
-				final Map<Direction, BlockPartFace> mapFacesIn = Maps.newEnumMap(Direction.class);
+					BlockFaceUV uvface = new BlockFaceUV(ModelUtils.modUV(ModelUtils.getUVs(partBoundary, face)), getFaceAngle(Axis.Y, face));
+					mapFacesIn.put(face, new BlockPartFace(null, -1, null, uvface));
 
-				final BlockFaceUV uvface = new BlockFaceUV(ModelUtils.modUV(ModelUtils.getUVs(partBoundary, face)), getFaceAngle(Axis.Y, face));
-				mapFacesIn.put(face, new BlockPartFace(null, -1, null, uvface));
+					BlockPart part = new BlockPart(limits[0], limits[1], mapFacesIn, null, true);
+					builder.addCulledFace(face, ModelUtils.makeBakedQuad(part, part.faces.get(face), bark, face, ModelRotation.X0_Y0, this.modelResLoc));
+				}
 
-				final BlockPart part = new BlockPart(limits[0], limits[1], mapFacesIn, null, true);
-				builder.addCulledFace(face, ModelUtils.makeBakedQuad(part, part.faces.get(face), bark, face, ModelRotation.X0_Y0, this.modelResLoc));
 			}
 		}
 
@@ -112,30 +109,30 @@ public class ThickBranchBlockBakedModel extends BasicBranchBlockBakedModel {
 	}
 
 	public IBakedModel bakeTrunkRings(int radius, TextureAtlasSprite ring, Direction face) {
-		final SimpleBakedModel.Builder builder = new SimpleBakedModel.Builder(this.blockModel.customData, ItemOverrideList.EMPTY).particle(ring);
-		final AxisAlignedBB wholeVolume = new AxisAlignedBB(8 - radius, 0, 8 - radius, 8 + radius, 16, 8 + radius);
-		final int wholeVolumeWidth = 48;
+		SimpleBakedModel.Builder builder = new SimpleBakedModel.Builder(this.blockModel.customData, ItemOverrideList.EMPTY).particle(ring);
+		AxisAlignedBB wholeVolume = new AxisAlignedBB(8 - radius, 0, 8 - radius, 8 + radius, 16, 8 + radius);
+		int wholeVolumeWidth = 48;
 
-		final ArrayList<Vector3i> offsets = new ArrayList<>();
+		ArrayList<Vector3i> offsets = new ArrayList<>();
 
 		for (Surround dir: Surround.values()) {
-			offsets.add(dir.getOffset()); // 8 surrounding component pieces.
+			offsets.add(dir.getOffset()); // 8 surrounding component pieces
 		}
 		offsets.add(new Vector3i(0, 0, 0)); // Center
 
-		for (final Vector3i offset : offsets) {
-			final Vector3d scaledOffset = new Vector3d(offset.getX() * 16, offset.getY() * 16, offset.getZ() * 16); // Scale the dimensions to match standard minecraft texels.
-			final AxisAlignedBB partBoundary = new AxisAlignedBB(0, 0, 0, 16, 16, 16).move(scaledOffset).intersect(wholeVolume);
+		for (Vector3i offset : offsets) {
+			Vector3d scaledOffset = new Vector3d(offset.getX() * 16, offset.getY() * 16, offset.getZ() * 16); // Scale the dimensions to match standard minecraft texels
+			AxisAlignedBB partBoundary = new AxisAlignedBB(0, 0, 0, 16, 16, 16).move(scaledOffset).intersect(wholeVolume);
 
-			final Vector3f posFrom = new Vector3f((float) partBoundary.minX, (float) partBoundary.minY, (float) partBoundary.minZ);
-			final Vector3f posTo = new Vector3f((float) partBoundary.maxX, (float) partBoundary.maxY, (float) partBoundary.maxZ);
+			Vector3f posFrom = new Vector3f((float) partBoundary.minX, (float) partBoundary.minY, (float) partBoundary.minZ);
+			Vector3f posTo = new Vector3f((float) partBoundary.maxX, (float) partBoundary.maxY, (float) partBoundary.maxZ);
 
-			final Map<Direction, BlockPartFace> mapFacesIn = Maps.newEnumMap(Direction.class);
-			final float textureOffsetX = -16f;
-			final float textureOffsetZ = -16f;
+			Map<Direction, BlockPartFace> mapFacesIn = Maps.newEnumMap(Direction.class);
+			float textureOffsetX = -16f;
+			float textureOffsetZ = -16f;
 
-			final float minX = ((float) ((partBoundary.minX - textureOffsetX) / wholeVolumeWidth)) * 16f;
-			final float maxX = ((float) ((partBoundary.maxX - textureOffsetX) / wholeVolumeWidth)) * 16f;
+			float minX = ((float) ((partBoundary.minX - textureOffsetX) / wholeVolumeWidth)) * 16f;
+			float maxX = ((float) ((partBoundary.maxX - textureOffsetX) / wholeVolumeWidth)) * 16f;
 			float minZ = ((float) ((partBoundary.minZ - textureOffsetZ) / wholeVolumeWidth)) * 16f;
 			float maxZ = ((float) ((partBoundary.maxZ - textureOffsetZ) / wholeVolumeWidth)) * 16f;
 
@@ -144,22 +141,22 @@ public class ThickBranchBlockBakedModel extends BasicBranchBlockBakedModel {
 				maxZ = ((float) ((partBoundary.minZ - textureOffsetZ) / wholeVolumeWidth)) * 16f;
 			}
 
-			final float[] uvs = new float[]{ minX, minZ, maxX, maxZ };
+			float[] uvs = new float[]{ minX, minZ, maxX, maxZ };
 
-			final BlockFaceUV uvface = new BlockFaceUV(uvs, getFaceAngle(Axis.Y, face));
+			BlockFaceUV uvface = new BlockFaceUV(uvs, getFaceAngle(Axis.Y, face));
 			mapFacesIn.put(face, new BlockPartFace(null, -1, null, uvface));
 
-			final BlockPart part = new BlockPart(posFrom, posTo, mapFacesIn, null, true);
+			BlockPart part = new BlockPart(posFrom, posTo, mapFacesIn, null, true);
 			builder.addCulledFace(face, ModelUtils.makeBakedQuad(part, part.faces.get(face), ring, face, ModelRotation.X0_Y0, this.modelResLoc));
 		}
 
 		return builder.build();
 	}
 
+	@Nonnull
 	@Override
 	public List<BakedQuad> getQuads(@Nullable final BlockState state, @Nullable final Direction side, final Random rand, final IModelData extraData) {
-		if (state == null || side != null)
-			return Collections.emptyList();
+		if (state == null || side != null) return Collections.emptyList();
 
 		int coreRadius = this.getRadius(state);
 
@@ -168,32 +165,31 @@ public class ThickBranchBlockBakedModel extends BasicBranchBlockBakedModel {
 
 		coreRadius = MathHelper.clamp(coreRadius, 9, 24);
 
-		final List<BakedQuad> quads = new ArrayList<>(30);
+		List<BakedQuad> quads = new ArrayList<>(30);
 
 		int[] connections = new int[] {0,0,0,0,0,0};
 		Direction forceRingDir = null;
-		final AtomicInteger twigRadius = new AtomicInteger(1);
+		int twigRadius = 1;
 
-		if (extraData instanceof ModelConnections) {
-			final ModelConnections connectionsData = (ModelConnections) extraData;
+		if (extraData instanceof ModelConnections){
+			ModelConnections connectionsData = (ModelConnections) extraData;
 			connections = connectionsData.getAllRadii();
 			forceRingDir = connectionsData.getRingOnly();
-
-			connectionsData.getFamily().ifValid(family ->
-					twigRadius.set(family.getPrimaryThickness()));
+			Family family = connectionsData.getFamily();
+			if (family.isValid()) twigRadius = family.getPrimaryThickness();
 		}
 
-		// Count number of connections.
+		//Count number of connections
 		int numConnections = 0;
-		for (int i: connections) {
-			numConnections += (i != 0) ? 1 : 0;
+		for(int i: connections) {
+			numConnections += (i != 0) ? 1: 0;
 		}
 
-		if (numConnections == 0 && forceRingDir != null) {
+		if (numConnections == 0 && forceRingDir != null){
 			return quads;
 		}
 
-		if (forceRingDir != null) {
+		if (forceRingDir != null){
 			connections[forceRingDir.get3DDataValue()] = 0;
 			quads.addAll(this.trunksBotRings[coreRadius - 9].getQuads(state, forceRingDir, rand, extraData));
 		}
@@ -201,7 +197,7 @@ public class ThickBranchBlockBakedModel extends BasicBranchBlockBakedModel {
 		for (Direction face : Direction.values()) {
 			quads.addAll(this.trunksBark[coreRadius - 9].getQuads(state, face, rand, extraData));
 			if (face == Direction.UP || face == Direction.DOWN) {
-				if (connections[face.get3DDataValue()] < twigRadius.get()) {
+				if (connections[face.get3DDataValue()] < twigRadius) {
 					quads.addAll(this.trunksTopRings[coreRadius - 9].getQuads(state, face, rand, extraData));
 				} else if (connections[face.get3DDataValue()] < coreRadius) {
 					quads.addAll(this.trunksTopBark[coreRadius - 9].getQuads(state, face, rand, extraData));
