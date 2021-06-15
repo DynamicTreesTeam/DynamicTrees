2ed9bc4e64270028596b5dba6bac1ea528fa3830
supermassimo <massimocantugarcia@gmail.com>
2021-06-15 01:34:36 -0300

    added the option to override selectNewDirection in growth logic kits

--- src/main/java/com/ferreusveritas/dynamictrees/trees/Species.java
+++ src/main/java/com/ferreusveritas/dynamictrees/trees/Species.java
@@ -1367,6 +1367,9 @@ public class Species extends RegistryEntry<Species> implements IResettable<Speci
 	 * @return The selected {@link Direction}.
 	 */
 	public Direction selectNewDirection(World world, BlockPos pos, BranchBlock branch, GrowSignal signal) {
+		Direction growthLogicDir = getGrowthLogicKit().selectNewDirection(world, pos, branch, signal);
+		if (growthLogicDir != null) return growthLogicDir; //if the growth logic kit overrides selectNewDirection, use that
+
 		Direction originDir = signal.dir.getOpposite();
 		
 		// prevent branches on the ground
