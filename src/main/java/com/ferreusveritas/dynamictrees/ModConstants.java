6cb77ccc9e073d71e78b34a9fe65eda85dffaf7f
Harleyoc1 <harley.oconnor@icloud.com>
2021-08-02 15:20:16 +0100

    Updated gradle script to use FG3.

--- src/main/java/com/ferreusveritas/dynamictrees/ModConstants.java
+++ src/main/java/com/ferreusveritas/dynamictrees/ModConstants.java
@@ -4,7 +4,7 @@ public class ModConstants {
 	
 	public static final String MODID = "dynamictrees";
 	public static final String NAME = "Dynamic Trees";
-	public static final String VERSION = "1.12.2-9999.9999.9999z";//Maxed out version to satisfy dependencies during dev, Assigned from gradle during build, do not change!
+	public static final String VERSION = "@VERSION@"; // Assigned from gradle during compile, do not change!
 	
 	public static final String OPTAFTER = "after:";
 	public static final String OPTBEFORE = "before:";
