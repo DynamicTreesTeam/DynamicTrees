5e36b128d8313dc7845ccc36792accb48bc263f5
Harleyoc1 <harley.oconnor@icloud.com>
2022-01-10 19:09:06 +0000

    Properly merge selector operators

--- src/main/java/com/ferreusveritas/dynamictrees/deserialisation/BiomeListDeserialiser.java
+++ src/main/java/com/ferreusveritas/dynamictrees/deserialisation/BiomeListDeserialiser.java
@@ -5,6 +5,7 @@ import com.ferreusveritas.dynamictrees.deserialisation.result.JsonResult;
 import com.ferreusveritas.dynamictrees.deserialisation.result.Result;
 import com.ferreusveritas.dynamictrees.util.BiomeList;
 import com.ferreusveritas.dynamictrees.util.JsonMapWrapper;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import net.minecraft.util.RegistryKey;
@@ -22,17 +23,21 @@ public final class BiomeListDeserialiser implements JsonDeserialiser<BiomeList> 
 
     private static final VoidApplier<BiomeList, String> TYPE_APPLIER = (biomeList, typeString) ->
             biomeList.removeIf(biome -> {
-                final Set<BiomeDictionary.Type> biomeTypes = BiomeDictionary.getTypes(RegistryKey.create(ForgeRegistries.Keys.BIOMES, biome.getRegistryName()));
-                if (typeString.toCharArray()[0] == '!') {
-                    return biomeTypes.stream().anyMatch(type -> type.toString().toLowerCase().matches(typeString.substring(1).toLowerCase()));
-                } else {
-                    return biomeTypes.stream().noneMatch(type -> type.toString().toLowerCase().matches(typeString.toLowerCase()));
-                }
-            });
+                        final Set<BiomeDictionary.Type> biomeTypes = BiomeDictionary.getTypes(
+                                RegistryKey.create(ForgeRegistries.Keys.BIOMES, biome.getRegistryName()));
+                        if (typeString.toCharArray()[0] == '!') {
+                            return biomeTypes.stream().anyMatch(
+                                    type -> type.toString().toLowerCase().matches(typeString.substring(1).toLowerCase()));
+                        } else {
+                            return biomeTypes.stream()
+                                    .noneMatch(type -> type.toString().toLowerCase().matches(typeString.toLowerCase()));
+                        }
+                    }
+            );
 
     private static final VoidApplier<BiomeList, String> CATEGORY_APPLIER = (biomeList, categoryString) ->
             biomeList.removeIf(biome -> {
-                final String biomeName = String.valueOf(biome.getRegistryName());
+                final String biomeName = biome.getRegistryName().toString();
                 if (categoryString.toCharArray()[0] == '!') {
                     return biomeName.toLowerCase().matches(categoryString.substring(1).toLowerCase());
                 } else {
@@ -50,6 +55,40 @@ public final class BiomeListDeserialiser implements JsonDeserialiser<BiomeList> 
                 }
             });
 
+    private static final VoidApplier<BiomeList, JsonArray> NAMES_OR_APPLIER = (biomeList, arrayList) -> {
+        BiomeList whitelist = new BiomeList();
+        BiomeList blacklist = new BiomeList();
+        BiomeList.getAll().forEach((biome -> {
+            final String biomeName = String.valueOf(biome.getRegistryName());
+            arrayList.forEach((jsonElement) -> {
+                String nameString = jsonElement.getAsString();
+                if (nameString.toCharArray()[0] == '!') {
+                    if (biomeName.matches(nameString.substring(1).toLowerCase())) {
+                        blacklist.add(biome);
+                    }
+                } else {
+                    if (biomeName.matches(nameString.toLowerCase())) {
+                        whitelist.add(biome);
+                    }
+                }
+            });
+        }));
+        whitelist.removeIf(blacklist::contains);
+        biomeList.removeIf((biome) -> !whitelist.contains(biome));
+    };
+
+    private final VoidApplier<BiomeList, JsonArray> orOperator = (biomes, jsonArray) -> {
+        BiomeList biomesList = new BiomeList();
+        for (JsonElement elem : jsonArray) {
+            if (elem instanceof JsonObject) {
+                BiomeList thisBiomesList = BiomeList.getAll();
+                applyAllAppliers(elem.getAsJsonObject(), thisBiomesList);
+                biomesList.addAll(thisBiomesList);
+            }
+        }
+        biomes.removeIf(biome -> !biomesList.contains(biome));
+    };
+
     private final JsonPropertyAppliers<BiomeList> appliers = new JsonPropertyAppliers<>(BiomeList.class);
 
     public BiomeListDeserialiser() {
@@ -57,7 +96,15 @@ public final class BiomeListDeserialiser implements JsonDeserialiser<BiomeList> 
                 .registerArrayApplier("types", String.class, TYPE_APPLIER)
                 .register("category", String.class, CATEGORY_APPLIER)
                 .register("name", String.class, NAME_APPLIER)
-                .registerArrayApplier("names", String.class, NAME_APPLIER);
+                .registerArrayApplier("names", String.class, NAME_APPLIER)
+                .register("names_or", JsonArray.class, NAMES_OR_APPLIER)
+                .registerArrayApplier("AND", JsonObject.class,
+                        (biomes, jsonObject) -> applyAllAppliers(jsonObject, biomes))
+                .register("OR", JsonArray.class, orOperator);
+    }
+
+    private void applyAllAppliers(JsonObject json, BiomeList biomes) {
+        appliers.applyAll(new JsonMapWrapper(json), biomes);
     }
 
     @Override
@@ -68,8 +115,7 @@ public final class BiomeListDeserialiser implements JsonDeserialiser<BiomeList> 
                     // Start with a list of all biomes.
                     final BiomeList biomes = BiomeList.getAll();
                     // Apply from all appliers, filtering the list.
-                    this.appliers.applyAll(new JsonMapWrapper(selectorObject), biomes);
-
+                    applyAllAppliers(selectorObject, biomes);
                     return biomes;
                 }).elseTypeError();
     }
