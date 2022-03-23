ec4268895b1e578cd229c5318cfafacbc95167a4
Max Hyper <massimocantugarcia@gmail.com>
2022-03-23 14:03:18 -0300

    Added Fruits and Pods to the registry list

--- src/main/java/com/ferreusveritas/dynamictrees/api/registry/Registries.java
+++ src/main/java/com/ferreusveritas/dynamictrees/api/registry/Registries.java
@@ -5,7 +5,9 @@ import com.ferreusveritas.dynamictrees.blocks.leaves.LeavesProperties;
 import com.ferreusveritas.dynamictrees.blocks.rootyblocks.SoilProperties;
 import com.ferreusveritas.dynamictrees.growthlogic.GrowthLogicKit;
 import com.ferreusveritas.dynamictrees.systems.dropcreators.DropCreator;
+import com.ferreusveritas.dynamictrees.systems.fruit.Fruit;
 import com.ferreusveritas.dynamictrees.systems.genfeatures.GenFeature;
+import com.ferreusveritas.dynamictrees.systems.pod.Pod;
 import com.ferreusveritas.dynamictrees.trees.Family;
 import com.ferreusveritas.dynamictrees.trees.Species;
 
@@ -30,7 +32,9 @@ public final class Registries {
                     GenFeature.REGISTRY,
                     DropCreator.REGISTRY,
                     Species.REGISTRY,
-                    SoilProperties.REGISTRY
+                    SoilProperties.REGISTRY,
+                    Fruit.REGISTRY,
+                    Pod.REGISTRY
             )
     );
 
