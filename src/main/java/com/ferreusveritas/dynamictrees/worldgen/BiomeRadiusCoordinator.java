ba318323c2c8933deeca5d07a4f21c8229a10a00
Harleyoc1 <harley.oconnor@icloud.com>
2022-06-15 10:35:08 +0100

    Fix noise density for Poisson radius selection

--- src/main/java/com/ferreusveritas/dynamictrees/worldgen/BiomeRadiusCoordinator.java
+++ src/main/java/com/ferreusveritas/dynamictrees/worldgen/BiomeRadiusCoordinator.java
@@ -38,7 +38,7 @@ public class BiomeRadiusCoordinator implements RadiusCoordinator {
         final double scale = 128; // Effectively scales up the noisemap
         final Biome biome = this.world.getUncachedNoiseBiome((x + 8) >> 2, 0, (z + 8) >> 2); // Placement is offset by +8,+8
 
-        final double noiseDensity = (this.noiseGenerator.getSurfaceNoiseValue(x / scale, 0, z / scale, 1.0) + 1D) / 2.0D; // Gives 0.0 to 1.0
+        final double noiseDensity = (this.noiseGenerator.getValue(x / scale, z / scale, false) + 1D) / 2.0D; // Gives 0.0 to 1.0
         final double density = BiomeDatabases.getDimensionalOrDefault(this.dimRegName)
                 .getDensitySelector(biome).getDensity(this.world.getRandom(), noiseDensity);
         final double size = ((1.0 - density) * 9); // Size is the inverse of density (gives 0 to 9)
