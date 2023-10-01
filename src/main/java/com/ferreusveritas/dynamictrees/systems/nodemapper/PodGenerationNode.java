b66cb875da1dbae566757f8fbc2a11fe6c04fe82
Massimo Cantú <massimocantugarcia@gmail.com>
2023-10-01 17:37:23 -0300

    added "lowest_trunk_height" property to pods

--- src/main/java/com/ferreusveritas/dynamictrees/systems/nodemapper/PodGenerationNode.java
+++ src/main/java/com/ferreusveritas/dynamictrees/systems/nodemapper/PodGenerationNode.java
@@ -3,6 +3,7 @@ package com.ferreusveritas.dynamictrees.systems.nodemapper;
 import com.ferreusveritas.dynamictrees.api.TreeHelper;
 import com.ferreusveritas.dynamictrees.api.network.NodeInspector;
 import com.ferreusveritas.dynamictrees.block.branch.BranchBlock;
+import com.ferreusveritas.dynamictrees.block.branch.TrunkShellBlock;
 import com.ferreusveritas.dynamictrees.systems.pod.Pod;
 import com.ferreusveritas.dynamictrees.util.CoordUtils;
 import net.minecraft.core.BlockPos;
@@ -23,12 +24,16 @@ public class PodGenerationNode implements NodeInspector {
     @Nullable
     private final Float seasonValue;
     private final int blocksPerPlacedPod;
+    private final BlockPos rootPos;
+    private final int lowestTrunkHeight;
 
-    public PodGenerationNode(Pod pod, PodPlacer podPlacer, @Nullable Float seasonValue, int blocksPerPlacedPods) {
+    public PodGenerationNode(Pod pod, PodPlacer podPlacer, @Nullable Float seasonValue, int blocksPerPlacedPods, BlockPos rootPos, int lowestTrunkHeight) {
         this.podPlacer = podPlacer;
         this.seasonValue = seasonValue;
         this.blocksPerPlacedPod = blocksPerPlacedPods;
         this.pod = pod;
+        this.rootPos = rootPos;
+        this.lowestTrunkHeight = lowestTrunkHeight;
     }
 
     private boolean finished = false;
@@ -37,31 +42,27 @@ public class PodGenerationNode implements NodeInspector {
     public boolean run(BlockState state, LevelAccessor level, BlockPos pos, Direction fromDir) {
 
         if (!finished) {
-            //int hashCode = CoordUtils.coordHashCode(pos, 1);
-            //if ((hashCode % 97) % blocksPerPlacedPod == 0) {
-                BranchBlock branch = TreeHelper.getBranch(state);
-                if (branch != null) {
-                    int radius = branch.getRadius(state);
-                    if (pod.isValidRadius(radius)){
-                        //int side = (hashCode % 4) + 2;
-                        //Direction dir = Direction.from3DDataValue(side);
-                        for (Direction dir : Direction.Plane.HORIZONTAL){
-                            int hashCode = CoordUtils.coordHashCode(pos.offset(dir.getNormal()), 1);
-                            if ((hashCode % 97) % blocksPerPlacedPod == 0) {
-                                BlockPos deltaPos = pos.relative(dir);
-                                if (level.isEmptyBlock(deltaPos)) {
-                                    if (!dir.getAxis().isHorizontal()) {
-                                        dir = Direction.NORTH;
-                                    }
-                                    podPlacer.place(level, deltaPos, seasonValue, dir.getOpposite(), radius);
-                                    finished = true;
+            if (pos.getY() - rootPos.above().getY() < lowestTrunkHeight) return false;
+            BranchBlock branch = TreeHelper.getBranch(state);
+            if (branch != null) {
+                int radius = branch.getRadius(state);
+                if (pod.isValidRadius(radius)){
+                    for (Direction dir : Direction.Plane.HORIZONTAL){
+                        int hashCode = CoordUtils.coordHashCode(pos.offset(dir.getNormal()), 1);
+                        if ((hashCode % 97) % blocksPerPlacedPod == 0) {
+                            BlockPos deltaPos = pos.relative(dir);
+                            if (level.isEmptyBlock(deltaPos) || level.getBlockState(deltaPos).getBlock() instanceof TrunkShellBlock) {
+                                if (!dir.getAxis().isHorizontal()) {
+                                    dir = Direction.NORTH;
                                 }
+                                podPlacer.place(level, deltaPos, seasonValue, dir.getOpposite(), radius);
+                                finished = true;
                             }
-
                         }
+
                     }
                 }
-            //}
+            }
         }
         return false;
     }
