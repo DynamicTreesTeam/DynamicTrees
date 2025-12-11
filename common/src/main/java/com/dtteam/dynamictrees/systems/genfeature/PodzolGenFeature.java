f9a3d24ebc420414c847d4def53580c2a0486f8c
Max Hyper <massimocantugarcia@gmail.com>
2025-12-11 14:52:31 -0300

    added podzol gen feature parameters

--- common/src/main/java/com/dtteam/dynamictrees/systems/genfeature/PodzolGenFeature.java
+++ common/src/main/java/com/dtteam/dynamictrees/systems/genfeature/PodzolGenFeature.java
@@ -1,7 +1,9 @@
 package com.dtteam.dynamictrees.systems.genfeature;
 
+import com.dtteam.dynamictrees.api.configuration.ConfigurationProperty;
 import com.dtteam.dynamictrees.api.network.MapSignal;
 import com.dtteam.dynamictrees.block.branch.BranchBlock;
+import com.dtteam.dynamictrees.block.branch.TrunkShellBlock;
 import com.dtteam.dynamictrees.block.soil.SoilBlock;
 import com.dtteam.dynamictrees.block.soil.SoilHelper;
 import com.dtteam.dynamictrees.platform.Services;
@@ -9,6 +11,7 @@ import com.dtteam.dynamictrees.platform.services.IConfigHelper;
 import com.dtteam.dynamictrees.systems.genfeature.context.PostGrowContext;
 import com.dtteam.dynamictrees.systems.nodemapper.FindEndsNode;
 import com.dtteam.dynamictrees.tree.TreeHelper;
+import com.dtteam.dynamictrees.tree.species.Species;
 import com.dtteam.dynamictrees.utility.CoordUtils;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -23,38 +26,52 @@ import java.util.List;
 
 public class PodzolGenFeature extends GenFeature {
 
+    public static final ConfigurationProperty<Block> PODZOL_BLOCK = ConfigurationProperty.block("podzol_block");
+    public static final ConfigurationProperty<Integer> SPREAD_DISTANCE = ConfigurationProperty.integer("spread_distance");
+    public static final ConfigurationProperty<Integer> DARK_THRESHOLD = ConfigurationProperty.integer("dark_threshold");
+    public static final ConfigurationProperty<Integer> MAX_HEIGHT = ConfigurationProperty.integer("max_height");
+    public static final ConfigurationProperty<Boolean> KILL_PLANTS = ConfigurationProperty.bool("kill_plants");
+
     public PodzolGenFeature(ResourceLocation registryName) {
         super(registryName);
     }
 
     @Override
     protected void registerProperties() {
+        register(KILL_PLANTS, DARK_THRESHOLD, SPREAD_DISTANCE, PODZOL_BLOCK, MAX_HEIGHT);
+    }
+
+    @Override
+    protected GenFeatureConfiguration createDefaultConfiguration() {
+        return super.createDefaultConfiguration()
+                .with(KILL_PLANTS, false)
+                .with(DARK_THRESHOLD, 10)
+                .with(SPREAD_DISTANCE, 2)
+                .with(PODZOL_BLOCK, Blocks.PODZOL)
+                .with(MAX_HEIGHT, 32);
     }
 
     @Override
     protected boolean postGrow(GenFeatureConfiguration configuration, PostGrowContext context) {
-        if (!Services.CONFIG.getBoolConfig(IConfigHelper.GENERATE_PODZOL)) {
-            return false;
-        }
+        if (!Services.CONFIG.getBoolConfig(IConfigHelper.GENERATE_PODZOL)) return false;
 
         final LevelAccessor level = context.level();
         final FindEndsNode endFinder = new FindEndsNode();
         TreeHelper.startAnalysisFromRoot(level, context.pos(), new MapSignal(endFinder));
         final List<BlockPos> endPoints = endFinder.getEnds();
-
-        if (endPoints.isEmpty()) {
-            return false;
-        }
+        if (endPoints.isEmpty()) return false;
 
         final RandomSource random = context.random();
         final BlockPos pos = endPoints.get(random.nextInt(endPoints.size()));
 
-        final int x = pos.getX() + random.nextInt(5) - 2;
-        final int z = pos.getZ() + random.nextInt(5) - 2;
+        int distance = configuration.get(SPREAD_DISTANCE);
+        final int x = pos.getX() + random.nextInt(distance * 2 + 1) - distance;
+        final int z = pos.getZ() + random.nextInt(distance * 2 + 1) - distance;
 
-        final int darkThreshold = 10;
-
-        for (int i = 0; i < 32; i++) {
+        final int darkThreshold = configuration.get(DARK_THRESHOLD);
+        Block podzol = configuration.get(PODZOL_BLOCK);
+        int maxHeight = configuration.get(MAX_HEIGHT);
+        for (int i = 0; i < maxHeight; i++) {
             final BlockPos offPos = new BlockPos(x, pos.getY() - 1 - i, z);
 
             if (!level.isEmptyBlock(offPos)) {
@@ -64,9 +81,9 @@ public class PodzolGenFeature extends GenFeature {
                     break; //Don't try to turn rooty soil into podzol.
                 }
                 // Skip past Mushrooms and branches on the way down.
-                if (block instanceof BranchBlock || block instanceof MushroomBlock || block instanceof LeavesBlock) {
+                if (block instanceof BranchBlock || block instanceof MushroomBlock || block instanceof LeavesBlock || block instanceof TrunkShellBlock) {
                     continue;
-                } else if (block instanceof FlowerBlock || block instanceof TallGrassBlock || block instanceof DoublePlantBlock) {
+                } else if (configuration.get(KILL_PLANTS) && (block instanceof FlowerBlock || block instanceof TallGrassBlock || block instanceof DoublePlantBlock)) {
                     // Kill plants.
                     if (level.getBrightness(LightLayer.SKY, offPos) <= darkThreshold) {
                         level.setBlock(pos, Blocks.AIR.defaultBlockState(), Block.UPDATE_ALL);
@@ -75,9 +92,9 @@ public class PodzolGenFeature extends GenFeature {
                 } else if (SoilHelper.isSoilAcceptable(state, SoilHelper.getSoilFlags(SoilHelper.DIRT_LIKE))) {
                     // Convert grass or dirt to podzol.
                     if (level.getBrightness(LightLayer.SKY, offPos.above()) <= darkThreshold) {
-                        level.setBlock(offPos, Blocks.PODZOL.defaultBlockState(), Block.UPDATE_ALL);
+                        level.setBlock(offPos, podzol.defaultBlockState(), Block.UPDATE_ALL);
                     } else {
-                        spreadPodzol(level, pos);
+                        spreadPodzol(level, pos, podzol);
                     }
                 }
                 break;
@@ -86,16 +103,16 @@ public class PodzolGenFeature extends GenFeature {
         return true;
     }
 
-    public static void spreadPodzol(LevelAccessor level, BlockPos pos) {
+    public static void spreadPodzol(LevelAccessor level, BlockPos pos, Block podzol) {
         int podzolish = 0;
 
         for (Direction dir : CoordUtils.HORIZONTALS) {
             BlockPos deltaPos = pos.relative(dir);
             Block testBlock = level.getBlockState(deltaPos).getBlock();
-            podzolish += (testBlock == Blocks.PODZOL) ? 1 : 0;
+            podzolish += (testBlock == podzol) ? 1 : 0;
             podzolish += testBlock instanceof SoilBlock ? 1 : 0;
             if (podzolish >= 3) {
-                level.setBlock(pos, Blocks.PODZOL.defaultBlockState(), Block.UPDATE_ALL);
+                level.setBlock(pos, podzol.defaultBlockState(), Block.UPDATE_ALL);
                 break;
             }
         }
