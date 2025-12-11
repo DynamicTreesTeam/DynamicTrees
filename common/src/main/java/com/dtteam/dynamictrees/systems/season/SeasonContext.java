5086049deaa0ec0940cb7a7ab200927a4f644853
Max Hyper <massimocantugarcia@gmail.com>
2025-12-11 12:16:58 -0300

    cached season factors

--- common/src/main/java/com/dtteam/dynamictrees/systems/season/SeasonContext.java
+++ common/src/main/java/com/dtteam/dynamictrees/systems/season/SeasonContext.java
@@ -11,9 +11,9 @@ import java.util.function.BiFunction;
 public class SeasonContext {
     private final SeasonProvider provider;
     private final SeasonGrowthCalculator calculator;
-//    private HashMap<ClimateZoneType, HashMap<Integer,Float>> cachedGrowthFactors = new HashMap<>();
-//    private HashMap<ClimateZoneType, HashMap<Integer,Float>> cachedSeedDropFactors = new HashMap<>();
-//    private HashMap<ClimateZoneType, HashMap<Integer,Float>> cachedFruitProductionFactors = new HashMap<>();
+    private final HashMap<ClimateZoneType, HashMap<Integer,Float>> cachedGrowthFactors = new HashMap<>();
+    private final HashMap<ClimateZoneType, HashMap<Integer,Float>> cachedSeedDropFactors = new HashMap<>();
+    private final HashMap<ClimateZoneType, HashMap<Integer,Float>> cachedFruitProductionFactors = new HashMap<>();
     private Float seasonValue;
 
     private final HashMap<ClimateZoneType, Float> peakFruitSeasons = new HashMap<>();
@@ -22,7 +22,6 @@ public class SeasonContext {
 
     private static final int updateRate = 20;
     private static final float seasonValueCacheMultiplier = 256;
-    private static final float wetSeasonOffset = 2f;
 
     public SeasonContext(SeasonProvider provider, SeasonGrowthCalculator calculator) {
         this.provider = provider;
@@ -37,26 +36,10 @@ public class SeasonContext {
         if (methodTicks % updateRate == 0) {
             provider.updateTick(world, dayTime);
             seasonValue = provider.getSeasonValue(world, BlockPos.ZERO);
-
-//            for (ClimateZoneType climate : ClimateZoneType.values()) {
-//                updateCache(cachedGrowthFactors, calculator::calcGrowthRate, climate, seasonValue);
-//                updateCache(cachedSeedDropFactors, calculator::calcSeedDropRate, climate, seasonValue);
-//                updateCache(cachedFruitProductionFactors, calculator::calcFruitProductionRate, climate, seasonValue);
-//            }
         }
 
         methodTicks++;
     }
-
-//    private void updateCache (HashMap<ClimateZoneType, HashMap<Integer,Float>> cache, BiFunction<Float, ClimateZoneType, Float> calculate, ClimateZoneType climate, float seasonVal){
-//        seasonVal %= 4;
-//        HashMap<Integer, Float> values = cache.getOrDefault(climate, null);
-//        if (values == null){
-//            cache.put(climate, new HashMap<>());
-//            values = cache.get(climate);
-//        }
-//        values.put((int)(seasonVal * seasonValueCacheMultiplier), calculate.apply(seasonVal, climate));
-//    }
 
     public SeasonProvider getSeasonProvider() {
         return provider;
@@ -68,17 +51,40 @@ public class SeasonContext {
 
     public float getGrowthFactor(float offset, ClimateZoneType climate) {
         if (seasonValue == null || climate == ClimateZoneType.NONE) return 1;
-        return calculator.calcGrowthRate(seasonValue + offset, climate);
+        float season = seasonValue + offset;
+        int key = (int)(season * seasonValueCacheMultiplier);
+
+        HashMap<Integer, Float> values = cachedGrowthFactors.computeIfAbsent(climate, k -> new HashMap<>());
+        if (!values.containsKey(key)){
+            float calculated = calculator.calcGrowthRate(season, climate);
+            values.put(key, calculated);
+        }
+        return values.get(key);
     }
 
     public float getSeedDropFactor(float offset, ClimateZoneType climate) {
-        if (seasonValue == null || climate == ClimateZoneType.NONE) return 1;
-        return calculator.calcSeedDropRate(seasonValue + offset, climate);
+        float season = seasonValue + offset;
+        int key = (int)(season * seasonValueCacheMultiplier);
+
+        HashMap<Integer, Float> values = cachedSeedDropFactors.computeIfAbsent(climate, k -> new HashMap<>());
+        if (!values.containsKey(key)){
+            float calculated = calculator.calcSeedDropRate(season, climate);
+            values.put(key, calculated);
+        }
+        return values.get(key);
     }
 
     public float getFruitProductionFactor(float offset, ClimateZoneType climate) {
         if (seasonValue == null || climate == ClimateZoneType.NONE) return 1;
-        return calculator.calcFruitProductionRate(seasonValue + offset, climate);
+        float season = seasonValue + offset;
+        int key = (int)(season * seasonValueCacheMultiplier);
+
+        HashMap<Integer, Float> values = cachedFruitProductionFactors.computeIfAbsent(climate, k -> new HashMap<>());
+        if (!values.containsKey(key)){
+            float calculated = calculator.calcFruitProductionRate(season, climate);
+            values.put(key, calculated);
+        }
+        return values.get(key);
     }
 
     public Float getPeakFruitProductionSeasonValue(float offset, ClimateZoneType climate) {
