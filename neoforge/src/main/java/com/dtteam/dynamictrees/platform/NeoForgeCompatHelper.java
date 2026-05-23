407442c1209da40c28b56b92bb1d32cb90758a11
Massimo Cantu <massimocantugarcia@gmail.com>
2026-05-23 14:36:12 -0300

    added serene seasons provider

--- neoforge/src/main/java/com/dtteam/dynamictrees/platform/NeoForgeCompatHelper.java
+++ neoforge/src/main/java/com/dtteam/dynamictrees/platform/NeoForgeCompatHelper.java
@@ -2,6 +2,7 @@ package com.dtteam.dynamictrees.platform;
 
 import com.dtteam.dynamictrees.DynamicTrees;
 import com.dtteam.dynamictrees.compat.EclipticSeasonsProvider;
+import com.dtteam.dynamictrees.compat.SereneSeasonsProvider;
 import com.dtteam.dynamictrees.platform.services.ICompatHelper;
 
 public class NeoForgeCompatHelper implements ICompatHelper {
@@ -9,14 +10,11 @@ public class NeoForgeCompatHelper implements ICompatHelper {
     @Override
     public void registerSeasonProvider(String modId) {
         switch (modId){
-            case DynamicTrees.SERENE_SEASONS -> {
-                //SereneSeasonsSeasonProvider.registerSereneSeasonsProvider()
-            }
-            case DynamicTrees.ECLIPTIC_SEASONS -> {
-                EclipticSeasonsProvider.registerEclipticSeasonsProvider();
-            }
+            case DynamicTrees.SERENE_SEASONS ->
+                    SereneSeasonsProvider.registerSereneSeasonsProvider();
+            case DynamicTrees.ECLIPTIC_SEASONS ->
+                    EclipticSeasonsProvider.registerEclipticSeasonsProvider();
         }
-//        ;
     }
 
 }
